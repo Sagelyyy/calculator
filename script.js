7e11047b4e9fb907de60187981d528661c60137c
Chris <orj.tsuk@gmail.com>
2021-07-20 22:40:23 -0400

    Working on getting addition hooked up, working with the display is buggy.

--- script.js
+++ script.js
@@ -6,12 +6,35 @@ const display = document.querySelector('#display')
 let displayValue = 0
 display.textContent = displayValue
 
+let numArray = []
+
 document.addEventListener('click', function(event){
     if(event.target.closest('#digit')){
         if(display.textContent == 0){
             display.textContent = event.target.textContent
         }else{
             display.textContent += event.target.textContent
+        }
+    }
+    if(event.target.closest('#operator')){
+        switch(event.target.textContent){
+            case 'ac':
+                numArray.length = 0
+                display.textContent = 0
+                break;
+            case '+':
+                numArray.push(parseInt(display.textContent))
+                if(numArray.length == 1){
+                    display.textContent = 0
+                }else if(numArray.length > 1){
+                    display.textContent = 0
+                    first = numArray.shift()
+                    second = numArray.shift()
+                    newSum = add(first, second)
+                    numArray.push(newSum)
+                    display.textContent = newSum
+                }
+            break;
         }
     }
 })
